02194e81e7868134579d3ecb90f419f5b60b557d
Denis Mtv <Freeflop@outlook.com>
2022-11-01 17:19:51 +0300

    some changed

--- handlers/other.py
+++ handlers/other.py
@@ -1,15 +1,13 @@
 from aiogram import types, Dispatcher
-from create_bot import dp
 import json, string
 
 
-@dp.message_handler()
+def register_handlers_other(dp: Dispatcher):
+    dp.register_message_handler(echo_send)
+
+
 async def echo_send(message: types.Message):
     if {elem.lower().translate(str.maketrans("", "", string.punctuation)) for elem in message.text.split(" ")} \
             .intersection(set(json.load(open("cenzura.json")))) != set():
         await message.reply("Маты запрещены")
         await message.delete()
-
-
-def register_handlers_other(dp: Dispatcher):
-    dp.register_message_handler(echo_send)
